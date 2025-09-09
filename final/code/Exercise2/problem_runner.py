c298a1a22791052b340ac7d67fdc59a3d54f9573
UniAccLeo <a1853245@adelaide.edu.au>
2025-09-09 14:35:23 +0930

    files for ex2

--- final/code/Exercise2/problem_runner.py
+++ final/code/Exercise2/problem_runner.py
@@ -15,8 +15,8 @@ def problem_runner(mutation_function, fitness_function, budget = None, n_runs = 
   for r in range(n_runs):
     f_opt = sys.float_info.min
     x_opt = None
-    f = sys.float_info.min
-    x = np.random.randint(2, size = fitness_function.meta_data.n_variables)
+    x = np.random.randint(2, size=fitness_function.meta_data.n_variables)
+    f = fitness_function(x)
     for i in range(budget):
       x_new = mutation_function(x, fitness_function.meta_data.n_variables)
       f_new = fitness_function(x_new)
@@ -32,18 +32,42 @@ def problem_runner(mutation_function, fitness_function, budget = None, n_runs = 
   return f_opt, x_opt
 
 def random_search(x, size):
-  return np.random.randint(2, size)
+  return np.random.randint(2, size=size)
 
-def RLS(x, size):
+def rls(x, size):
   x_copy = x.copy()
   flip_idx = np.random.randint(size)
   x_copy[flip_idx] = 1 - x_copy[flip_idx]
   return x_copy
 
-def OnePlusOne(x, size):
+def one_plus_one(x, size):
   flip_chance = float(1/size)
   x_copy = x.copy()
   for s in range(size):
     if(np.random.rand() < flip_chance):
       x_copy[s] = 1- x_copy[s]
   return x_copy
+
+problemIds = [1, 2, 3, 18, 23, 24, 25]
+for pid in problemIds: 
+    for function in [random_search, rls, one_plus_one]:
+        problem = get_problem(
+            fid=pid,
+            dimension=100,
+            instance=1,
+            problem_class=ProblemClass.PBO
+        )
+        l = logger.Analyzer(
+        root="data",
+        folder_name=f"Exercise2/problem_{pid}_{function.__name__}",
+        algorithm_info="Exercise two",
+        algorithm_name=function.__name__
+        )   
+        problem.attach_logger(l)
+        problem_runner(
+            mutation_function=function,
+            fitness_function=problem,
+            budget=100000,
+            n_runs=10
+        )
+    del l

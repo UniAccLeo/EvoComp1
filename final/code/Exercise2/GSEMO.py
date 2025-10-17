d5370273c6c2253f2c4adb95cd832367e872c6b5
unknown <you@example.com>
2025-10-17 15:08:44 +1030

    ?

--- final/code/Exercise2/GSEMO.py
+++ final/code/Exercise2/GSEMO.py
@@ -1,8 +1,9 @@
 import ioh
+import random 
 
 f = ioh.get_problem(2100, problem_class=ioh.ProblemClass.GRAPH)
 
-def fitness_function(x, problem):
+def fitness(x, problem):
     """
     Multi-objective fitness:
     - objective 1: submodular value from IOHprofiler
@@ -13,3 +14,21 @@ def fitness_function(x, problem):
     return (objValue, -cost)
 
 
+def GSEMO(problem, budget=10000):
+    #choose x uniformly at random
+    num_variables = problem.n_variables
+    x = [0]*num_variables
+    for i in range(0, num_variables-1):
+        choice = random.randint(0,1)
+        x[i] = choice
+
+    #determine g(x)
+    g_x = fitness(x, problem)
+    
+    #define population
+    population = [(x, g_x)]
+    
+
+    
+        
+    
